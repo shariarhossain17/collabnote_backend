f1d4478327a5f5ff050259124f31251096c68682
shariar17 <shariarhossain23@gmail.com>
2026-04-21 15:33:11 +0600

    add table rules and args

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from sqlalchemy import Column, Integer, String, DateTime,UniqueConstraint,Boolean
 from .database import Base
 
 class User(Base):
@@ -10,5 +10,14 @@ class User(Base):
     email = Column(String(255), nullable=False, unique=True, index=True)
     username = Column(String(50), nullable=False, unique=True, index=True)
     pass_hash = Column(String(255), nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow)
+    created_at = Column(DateTime, default=datetime.utcnow())
     is_active = Column(Boolean, default=True)
+
+
+    __table_args__= (
+        UniqueConstraint("email",name="uq_user_email"),
+        UniqueConstraint("username",name="uq_user_name"),
+    )
+
+    def __repr__(self):
+        return f"<USer(id={self.id},email={self.email}),username={self.username})>"
